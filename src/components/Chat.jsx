3121bdce7d4b92aacfd0d045e37ad8a3e800b903
oleggkam <milleroleg99@yandex.ru>
2020-09-23 01:42:55 +0700

    Закончена верстка

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
 import '../css/Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>I AM A CHAT</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Oleg</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciver">
+          <span className="chat__name">Oleg</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <form>
+          <InsertEmoticonIcon />
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+          <MicIcon />
+        </form>
+      </div>
     </div>
   );
 }
